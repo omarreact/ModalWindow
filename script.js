0532f71c492e7819e6d1eec31e00ecd6cceb185a
Faruk Khan <fk8187990@gmail.com>
2022-10-12 22:56:15 +0600

    Trash

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const showModal = document.querySelectorAll('.show-modal');
 const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-let name = 'Faruk Khan';
-
 for (let i = 0; i < showModal.length; i++) {
   showModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
@@ -28,9 +26,3 @@ document.addEventListener('keydown', function (e) {
     overlay.classList.add('hidden');
   }
 });
-
-let marks = '7';
-marks = marks + '0';
-const results = Number(marks) + 3;
-// print(float(results));
-console.log(results);
